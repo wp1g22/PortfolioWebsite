2b046c7a6109ff33270d7dd732031bcb588a5626
wp1g22 <wp1g22@soton.ac.uk>
2023-08-09 18:07:16 +0100

    Fixed desktop nav, added red hover animation.

--- src/app/Toolbar/toolbar/toolbar.component.ts
+++ src/app/Toolbar/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButton, MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -6,6 +6,8 @@ import {MatMenuModule} from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 import { style, transition, trigger, animate, state } from '@angular/animations';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-toolbar',
@@ -14,48 +16,6 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
   standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, CommonModule, AppRoutingModule],
   animations: [
-    trigger(
-      'inOutAnimationHorizontal', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({width: 0, opacity: 0 }),
-            animate('0.5s ease-out', 
-                    style({ width: 51, opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({ width: 51, opacity: 1 }),
-            animate('0.5s ease-in', 
-                    style({ width: 0, opacity: 0}))
-          ]
-        )
-      ]
-    ),
-    trigger(
-      'inOutAnimationVertical', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({height: 0, opacity: 0 }),
-            animate('0.5s ease-out', 
-                    style({ height: 60, opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({ height: 60, opacity: 1 }),
-            animate('0.5s ease-in', 
-                    style({ height: 0, opacity: 0}))
-          ]
-        )
-      ]
-    ),
     trigger(
       'rotateAnimation',
       [
@@ -71,12 +31,12 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
           
           transition('open => closed', [
           style({rotate:'90deg'}),
-          animate('0.4s 00ms ease-in',style({
+          animate('0.5s 00ms ease-in',style({
           rotate:'0deg'}))]),
 
           transition('closed => open', [
             style({rotate:'0deg'}),
-            animate('0.4s 00ms ease-in',style({
+            animate('0.5s 00ms ease-in',style({
             rotate:'90deg'}))]),
       ]
     )
@@ -87,7 +47,66 @@ ngOnInit(): void {
   
 }
 hide = false;
+hover(num : number) {
+  // console.log('hover + o' + num)
+  const elements = document.querySelectorAll('.o' + num);
+  elements.forEach((element) => {
+    if(element.classList.contains('oHover')){
+      element.classList.remove('oHover');
+    }else{
+      element.classList.add('oHover');
+    }
+  });
+}
 toggleHome() {
+  const elements = document.querySelectorAll('.example-icon');
+  const elements2 = document.querySelectorAll('.row');
+  const elements3 = document.querySelectorAll('.menu');
+  const elements4 = document.querySelectorAll('.example-text');
+  const elements5 = document.querySelectorAll('.example-text-small');
+
+  elements3.forEach((element) => {
+    if(element.classList.contains('activeMenu')){
+      element.classList.remove('activeMenu');
+    }else{
+      element.classList.add('activeMenu');
+    }
+  });
+
+  elements.forEach((element) => {
+    if(element.classList.contains('activeIcon')){
+      element.classList.remove('activeIcon');
+    }else{
+      element.classList.add('activeIcon');
+    }
+  });
+
+  elements4.forEach((element) => {
+    if(element.classList.contains('activeText')){
+      element.classList.remove('activeText');
+    }else{
+      element.classList.add('activeText');
+    }
+  });
+
+  elements5.forEach((element) => {
+    if(element.classList.contains('activeTextSmall')){
+      element.classList.remove('activeTextSmall');
+    }else{
+      element.classList.add('activeTextSmall');
+    }
+  });
+
+  elements2.forEach((element) => {
+    if(element.classList.contains('activeRow')){
+      element.classList.remove('activeRow');
+      element.classList.add('nonActiveRow');
+    } else{
+      element.classList.remove('nonActiveRow');
+      element.classList.add('activeRow');
+    }
+  });
+
   this.hide = !this.hide;
 }
 }
