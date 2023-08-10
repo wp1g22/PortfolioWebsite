33a9b444140c9a4d67b0e2c847f8d9bbb3e514ee
wp1g22 <wp1g22@soton.ac.uk>
2023-08-10 20:34:13 +0100

    Education fade animation

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit{
+  ngOnInit(): void {
+    this.createObserver();
+  }
 
+  private createObserver() {
+    const options = {
+      rootMargin: '0px',
+      threshold: [0, 0.5],
+    };
+    const elements = document.querySelectorAll('.fade-in');
+    const isIntersecting = (entry: IntersectionObserverEntry) =>
+      entry.isIntersecting || entry.intersectionRatio > 0;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (isIntersecting(entry)) {
+          entry.target.classList.add('active');
+        }else{
+          entry.target.classList.remove('active');
+          entry.target.classList.remove('delay')
+        }
+      });
+    }, options);
+    elements.forEach(element => {
+      observer.observe(element);
+    });
+  }
 }
