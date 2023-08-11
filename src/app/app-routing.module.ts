8f23b54c046bdba4849d6182cde4af06187883e1
wp1g22 <wp1g22@soton.ac.uk>
2023-08-11 11:45:37 +0100

    Contact page. William author message.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { ExperienceComponent } from './experience/experience.component';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import { EducationComponent } from './education/education.component';
 import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path : 'home', component: HomeComponent},
   {path : 'experience', component: ExperienceComponent},
   {path : 'education', component: EducationComponent},
   {path : 'hobbies', component: HobbiesComponent},
+  {path : 'contact', component: ContactComponent},
   {path: '', redirectTo :'home', pathMatch: 'full'},
   { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
 ];
